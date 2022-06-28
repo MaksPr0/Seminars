07e94eb64d2dedccd2f750f2edd8009cb53b5af5
alimurtazaev <108363960+alimurtazaev@users.noreply.github.com>
2022-06-28 22:25:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Смотрин Николай: пишет в файл
-def write_in_txt_file():
-  with open('text.txt', 'w') as writer:
-    writer.write('I love Git')
-    return True
-
 import os
 # Муртазаев Абляким: удаляет файл
 def delete_file():
