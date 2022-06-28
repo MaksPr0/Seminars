ffe0444af1c50c2c67b91eb9c1a64d185d6ff0b0
alimurtazaev <108363960+alimurtazaev@users.noreply.github.com>
2022-06-28 21:55:11 +0300

    Создал удаление файла

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+# Муртазаев Абляким: удаляет файл
+def delete_file():
+  file_path = 'file.txt'
+  try:
+    os.remove(file_path)
+    return True
+  except:
+    print("The system cannot find the file specified")
+    return False
+ 
