144fb8b79a96de7723a891e5d8e2c1c82b6a5b1e
MaksPr0 <108338628+MaksPr0@users.noreply.github.com>
2022-06-28 21:09:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Смотрин Николай: пишет в файл
-def write_in_txt_file():
-  with open('text.txt', 'w') as writer:
-    writer.write('I love Git')
-    return True
 # Сидоров Станислав: читает с файла    
 def read_txt_file():
   file = 'file.txt'
